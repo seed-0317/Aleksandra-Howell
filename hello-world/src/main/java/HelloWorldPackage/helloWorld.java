7529ced6c65bea2321cc5f04c02aacab4d59e926
Howell <ues643@784f437c296f.cof.ds.capitalone.com>
2017-04-03 16:20:17 -0400

    Adding my java project to GitHub

---
+++ hello-world/src/main/java/HelloWorldPackage/helloWorld.java
@@ -0,0 +1,10 @@
+package HelloWorldPackage;
+
+/**
+ * Created by ues643 on 4/3/17.
+ */
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+}
